e4bb17a2a4b7e30e31e88ad7d277efe5ce16cd4e
diogenes-css <diogenesluiz2008@gmail.com>
2024-09-18 09:16:58 -0300

    Add files via upload

---
+++ 1s.js
@@ -0,0 +1,86 @@
+// produto
+var Smartphone= 2.499
+var FoneBluetooth= 599
+var SmartwatchUltra=1.299
+var TabletZenPad=899
+
+var Liquidificador=249
+var MáquinadeCafé=499
+var VentiladordeColuna= 179
+var Microondas=399
+
+var Camisa  =89
+var Calça  =159
+var Tênis = 299
+var Jaqueta = 499
+
+var Perfume= 199
+var CremeHidratante =129
+var KitdeMaquiagem = 299
+var EscovadeCabelo = 169
+
+var ConjuntodeToalhas=149
+var Abajur=99
+var Quadro=179
+var Almofada = 89
+
+var CarrinhodeControleRemoto= 149
+var Quebracabeça=79
+var Boneca = 199
+var JogodeTabuleiro = 129
+
+var Livro=59
+var DVDBox = 129
+var CD = 39
+var Ebook= 49
+
+var CestadeFrutas =89
+var Vinho= 149
+var CaixadeChocolates =79
+var Azeite= 59
+// valor
+var nSmartphone= 0
+var nFoneBluetooth=0
+var nSmartwatchUltra=0
+var nTabletZenPad=0
+
+var nLiquidificador=0
+var nMáquinadeCafé=0
+var nVentiladordeColuna= 0
+var nMicroondas=0
+
+var nCamisa  =0
+var nCalça  =0
+var nTênis = 0
+var nJaqueta = 0
+
+var nPerfume= 0
+var nCremeHidratante =0
+var nKitdeMaquiagem = 0
+var nEscovadeCabelo =0
+
+var nConjuntodeToalhas=0
+var nAbajur=0
+var nQuadro=0
+var nAlmofada = 0
+
+var nCarrinhodeControleRemoto=0
+var nQuebracabeça=0
+var nBoneca = 0
+var nJogodeTabuleiro = 0
+
+var nLivro=0
+var nDVDBox = 0
+var nCD = 0
+var nEbook= 0
+
+var nCestadeFrutas =0
+var nVinho= 0
+var nCaixadeChocolates =0
+var nAzeite= 0
+
+
+function adicionar() {
+    
+}
+
